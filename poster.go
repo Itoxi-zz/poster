afd3bcf82279928cacd9a220ae9f7ab94d11ac1d
Mike Seavers <mike.seavers@gmail.com>
2014-09-23 14:29:44 -0500

    All the files

--- poster.go
+++ poster.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Println("Received")
+	w.WriteHeader(200)
+	w.Write([]byte("foo"))
 }
 
 func main() {
+	pwd, _ := os.Getwd()
+	fs := http.FileServer(http.Dir(pwd))
+	http.Handle("/", http.StripPrefix("/", fs))
 	http.HandleFunc("/form/submitter", handler)
 	http.ListenAndServe(":9090", nil)
 }
